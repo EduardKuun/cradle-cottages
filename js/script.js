771cb5c14bf7397c3a624d81bbdc32fb7dcfc2cb
Eduard Kuun <eduardkuun@outlook.com>
2019-05-30 17:14:14 +0200

    General Update

--- js/script.js
+++ js/script.js
@@ -1,34 +1,40 @@
-/* NAVBAR */
-$(".navbar-nav>a").on("click", function () {
-  $(".navbar-collapse").collapse("hide");
-});
-
-window.onscroll = function () {
-  let scrolledFromTop = window.pageYOffset;
-  if (scrolledFromTop >= 70) {
-    document.querySelector('.navbar').style.cssText = "padding: 0 10px !important;";
-    document.querySelector('.navbar-brand img').style.cssText = "padding: 5px 10px !important;";
-    document.querySelectorAll('.nav-link').forEach(function(item){
-      item.style.padding = "40px .5rem 40px .5rem";
-    });
-  } else {
-    document.querySelector('.navbar').style.cssText = "padding: 15px 10px 0 10px !important;";
-    document.querySelector('.navbar-brand img').style.cssText = "padding: 5px 5px !important;";
-    document.querySelectorAll('.nav-link').forEach(function(item){
-      item.style.padding = "40px .5rem 50px .5rem";
-    });
-
+/*NAVBAR*/
+(function(){
+  window.onscroll = function () {
+    let scrolledFromTop = window.pageYOffset;
+    if (scrolledFromTop >= 70) {
+      document.querySelector('.navbar').style.cssText = "padding: 0 10px !important;";
+      document.querySelector('.navbar-brand img').style.cssText = "padding: 5px 10px !important;";
+      document.querySelectorAll('.nav-link').forEach(function(item){
+        item.style.padding = "40px .5rem 40px .5rem";
+      });
+    } else {
+      document.querySelector('.navbar').style.cssText = "padding: 15px 10px 0 10px !important;";
+      document.querySelector('.navbar-brand img').style.cssText = "padding: 5px 5px !important;";
+      document.querySelectorAll('.nav-link').forEach(function(item){
+        item.style.padding = "40px .5rem 50px .5rem";
+      });
+  
+    }
   }
-}
-
-/*LIGHTBOX*/
-lightbox.option({
-  'disableScrolling': true,
-  'wrapAround': true
-});
-
+}());
 
 /*COPYRIGHT YEAR*/
-var currentDate = new Date();
-var currentYear = currentDate.getFullYear();
-document.getElementById('currentYear').textContent = currentYear;
+(function(){
+  var currentDate = new Date();
+  var currentYear = currentDate.getFullYear();
+  document.getElementById('currentYear').textContent = currentYear;
+}());
+
+
+/*LIGHTBOX*/
+(function(){
+  try {
+    lightbox.option({
+      'disableScrolling': true,
+      'wrapAround': true
+    });
+  } catch (error) {
+    console.log('Lightbox not on this page.');
+  }
+}());
